1a4ea59b8281193f31de3f0dd175ad0e258d1f62
Alex G <admin@mg100.net>
2022-08-20 10:29:29 +0200

    Create Pterodactyl.php

---
+++ src/hmcsw4/Resources/Service/Pterodactyl.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace HMCSW4\Client\Resources\Service;
+
+use HMCSW4\Client\Request;
+use HMCSW4\Client\Resources\Service;
+use HMCSW4\Client\Resources\Resource;
+
+class Pterodactyl extends Service
+{
+  public function __construct ($HMCSW, int $team_id, int $service_id)
+  {
+    parent::__construct($HMCSW, $team_id, $service_id);
+    $this->team_id = $team_id;
+    $this->service_id = $service_id;
+  }
+
+  public function get(){
+    return $this->HMCSW4->getRequest()->get("user/teams/".$this->team_id."/services/".$this->service_id."/info");
+  }
+
+}
